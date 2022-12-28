6390e1d220e46265712208ec44860b9830d91ae3
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2022-12-29 00:14:15 +0100

    Refactored modified code

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -4,11 +4,14 @@ import { Observable } from "rxjs";
 import { Employee } from "./employee";
 import { environment } from "src/environments/environment";
 
+/*the @Injectable decorator with the providedIn property is used to define and register a service in Angular. 
+It is a useful tool for organizing and structuring your code and for enabling dependency injection in your app.*/
+
 @Injectable({
-    providedIn: 'root'
+    providedIn: 'root' // By setting providedIn to 'root', you are indicating that the service should be available throughout the entire application and can be injected into any component or service that needs it.
 })
 export class EmployeeService {
-    private apiServerUrl = environment.apiBaseUrl;
+    private apiServerUrl: string = environment.apiBaseUrl;
 
     constructor(private http: HttpClient){}
 
@@ -20,10 +23,10 @@ export class EmployeeService {
         return this.http.post<Employee>(`${this.apiServerUrl}/employee/add`,employee);
     }
 
-    public updateEmployee(employee: Employee): Observable<Employee[]>{
-        return this.http.put<Employee[]>(`${this.apiServerUrl}/employee/update`,employee);
+    public updateEmployee(employee: Employee): Observable<Employee>{
+        return this.http.put<Employee>(`${this.apiServerUrl}/employee/update`,employee);
     }
-    public deleteEmployee(employeeId: number): void{
-        this.http.delete<void>(`${this.apiServerUrl}/employee/delete/${employeeId}`);
+    public deleteEmployee(employeeId: number): Observable<void>{
+        return this.http.delete<void>(`${this.apiServerUrl}/employee/delete/${employeeId}`);
     }
 }
